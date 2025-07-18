8bf8cc85be5f781f50a01732a2e8aefbbd3135cb
ghDecapy <josh.mci112@gmail.com>
2025-07-18 07:39:00 +0100

    Getting Parity innit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
+from enum import Enum
 from textnode import *
+from htmlnode import *
 
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
     return 
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.PLAIN_TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD_TEXT:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC_TEXT:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE_TEXTTEXT:
+        return LeafNode("code", text_node.text)
+
+
 
 main()
