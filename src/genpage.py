ad25c40bd66560fe26c3911af5c77df1ceffb5dc
ghDecapy <josh.mci112@gmail.com>
2025-07-24 09:26:43 +0100

    Nearly there

--- src/genpage.py
+++ src/genpage.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from inline_markdown import *
 from markdown_blocks import *
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path)
     markdown_content = from_file.read()
@@ -15,21 +15,23 @@ def generate_page(from_path, template_path, dest_path):
     html_file = html.to_html()
     title = extract_title(markdown_content)
     dest_file = template.replace("{{ Title }}", title).replace("{{ Content }}", html_file)
+    dest_file = template.replace('href="/', 'href="' + basepath)
+    dest_file = template.replace('src="/', 'src="' + basepath)
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(dest_file)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 def extract_title(markdown):
     lines = markdown.splitlines()
